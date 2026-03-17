5d2510722f1e0ca051f568e5be86f1dba28643d6
Pallav <pallavvashist11@gmail.com>
2026-03-18 00:33:32 +0530

    fix

--- index.js
+++ index.js
@@ -223,10 +223,6 @@ app.get("/books", authenticateToken, async (req, res) => {
   }
 });
 
-/* =========================
-   SERVER
-========================= */
-
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });

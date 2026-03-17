f00256d25151bf34d1b086f9b2a7071fc81bab11
Pallav <pallavvashist11@gmail.com>
2026-03-18 01:23:43 +0530

    fix

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE, 
+  port: Number(process.env.MYSQLPORT) || 3306, 
   waitForConnections: true,
   connectionLimit: 10
 });
